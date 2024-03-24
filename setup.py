e38c6cf27bae1a9956adf9139bdc98b1c1cf4845
mjfactor <emjayfactor@gmail.com>
2024-03-24 18:26:28 +0800

    ver 2.3

--- setup.py
+++ setup.py
@@ -2,16 +2,14 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-
 HERE = path.abspath(path.dirname(__file__))
 
 with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-
 setup(
     name='boboBot',
-    version='2.2',
+    version='2.3',
     packages=find_packages(),
     author='mjfactor',
     author_email='emjayfactor@gmail.com',
@@ -22,7 +20,11 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.10',
         "Operating System :: OS Independent"
     ],
 )
